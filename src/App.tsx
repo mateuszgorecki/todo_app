6080062811426445010fc1898709f029025a3b3e
Mateusz Górecki <mateusz.gorecki.dev@gmail.com>
2023-05-10 12:49:55 +0200

    add mixins file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React, { useContext } from 'react'
 
+import Header from './components/Header'
+import Main from './components/Main'
 import Footer from './components/Footer'
 
 import './styles/_global.scss'
 import styles from './App.module.scss'
 
-import ThemeContext  from './components/context/theme-context'
+import ThemeContext from './components/context/theme-context'
 
 function App() {
-
   const ctx = useContext(ThemeContext)
 
   const classes = `${styles.app} ${ctx.isDarkTheme ? styles.dark : ''}`
 
   return (
     <div className={classes}>
-      <button onClick={ctx.changeTheme} >Toggle</button>
+      <button onClick={ctx.changeTheme}>Toggle</button>
+      <Header />
+      <Main />
       <Footer />
     </div>
   )
