9e1088437acd624f7a880fae6e0faaf001347d03
Mateusz Górecki <mateusz.gorecki.dev@gmail.com>
2023-05-11 16:15:26 +0200

    done light theme

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -4,40 +4,40 @@ interface Props {
   isDark: boolean
 }
 
-const DUMMY_DATA = [
+export const DUMMY_DATA = [
   {
     id: '1',
-    title: 'Task 1',
+    title: 'Complete online JS course',
     isCompleted: true,
   },
   {
     id: '2',
-    title: 'Task 2',
+    title: 'Jog around the park 3x',
     isCompleted: false,
   },
   {
     id: '3',
-    title: 'Task 3',
+    title: '10 minutes meditaion',
     isCompleted: false,
   },
   {
     id: '4',
-    title: 'Task 4',
+    title: 'Read for 1 hour',
     isCompleted: false,
   },
   {
     id: '5',
-    title: 'Task 5',
+    title: 'Pick up groceries',
     isCompleted: false,
   },
   {
     id: '6',
-    title: 'Task 6',
+    title: 'Complete Todo App on Frontend Mentor',
     isCompleted: false,
   },
 ]
 
-const TasksList = ({isDark}: Props) => {
+const TasksList = ({ isDark }: Props) => {
   return (
     <ul>
       {DUMMY_DATA.map(({ id, title, isCompleted }) => (
