d1572b490afc71ac694d02d77b5892b5af421201
Mateusz Górecki <mateusz.gorecki.dev@gmail.com>
2023-05-16 12:41:51 +0200

    set min & max height of tasks list and add info when no tasks

--- src/components/TasksList.tsx
+++ src/components/TasksList.tsx
@@ -2,25 +2,27 @@ import { useContext } from 'react'
 import TaskItem from './TaskItem'
 import TasksContext from './context/tasks-context'
 
+import classes from './TasksList.module.scss'
+
 interface Props {
   isDark: boolean
 }
 
 const TasksList = ({ isDark }: Props) => {
   const ctx = useContext(TasksContext)
-  const tasks = ctx.filteredTasks
+  const tasks = ctx.filteredTasks.map(({ id, title, isCompleted }) => (
+    <TaskItem
+      key={id}
+      id={id}
+      text={title}
+      isCompleted={isCompleted}
+      isDark={isDark}
+    />
+  ))
 
   return (
-    <ul>
-      {tasks.map(({ id, title, isCompleted }) => (
-        <TaskItem
-          key={id}
-          id={id}
-          text={title}
-          isCompleted={isCompleted}
-          isDark={isDark}
-        />
-      ))}
+    <ul className={classes.list}>
+      {tasks.length > 0 ? tasks : <p>No tasks!</p>}
     </ul>
   )
 }
