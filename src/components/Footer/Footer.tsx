57bb327109537b086740edd75e8fb0857e931cff
Mateusz Górecki <mateusz.gorecki.dev@gmail.com>
2023-05-17 14:55:41 +0200

    add link to repo in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,26 +1,37 @@
-
 import styles from './Footer.module.scss'
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Challenge by{' '}
-      <a
-        href='https://www.frontendmentor.io?ref=challenge'
-        target='_blank'
-        rel='noreferrer'
-      >
-        Frontend Mentor
-      </a>
-      . Coded by{' '}
-      <a
-        href='https://mateusz-gorecki.dev'
-        target='_blank'
-        rel='noreferrer'
-      >
-        Me
-      </a>
-      .
+      <p>
+        Challenge by{' '}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a
+          href='https://mateusz-gorecki.dev'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Me
+        </a>
+        .
+      </p>
+      <p>
+        Link to repo on{' '}
+        <a
+          href='https://github.com/mateuszgorecki/todo_app'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Github
+        </a>
+      </p>
     </footer>
   )
 }
